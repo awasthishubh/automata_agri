56be04fd2681ba04767b92971f2b6da998e70e6c
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-06 19:58:46 +0530

    Added predict map, select, loader, result

---
+++ js/defined.js
@@ -0,0 +1,11 @@
+function back(){
+    console.log('back')
+    $('#result').fadeOut()
+    $('#select').fadeIn()
+}
+function submit(){
+    console.log('submit')
+    $('#result').fadeIn()
+    $('#select').fadeOut()
+
+}
