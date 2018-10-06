d254cd9d681f3fa7f7cb5afd28764c99202e0b0b
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-06 21:35:56 +0530

    Added materialize and ui changs

--- js/defined.js
+++ js/defined.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+    $('select').formSelect();
+  });
 function back(){
     console.log('back')
     $('#result').fadeOut()
@@ -13,13 +16,27 @@ function submit(){
         beforeSend: function(){
             $('#select').fadeOut()
             $('#loader').fadeIn()
+            $('#card-color').removeClass('green')
+            $('#card-color').removeClass('red')
+            $('#lowYield').hide()
+            $('#normalYield').hide()
+            $('#highYield').hide()
         }
     }).done(function(data){
         console.log(data)
         $('#totalyield').html(data.yield)
-        if(data.type==0)  $('#lowYield').show()
-        if(data.type==1)  $('#normalYield').show()
-        if(data.type==2)  $('#highYield').show()
+        if(data.type==0){
+            $('#lowYield').show()
+            $('#card-color').addClass('red')
+        }
+        if(data.type==1){
+            $('#normalYield').show()
+            $('#card-color').addClass('green')
+        }
+        if(data.type==2){
+            $('#highYield').show()
+            $('#card-color').addClass('red')
+        }
         $('#loader').fadeOut('fast')
         $('#result').fadeIn('slow')
     }).catch(function(data){
