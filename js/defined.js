466276e833836e8d804385bb9a6d7083b7b89111
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-06 20:25:11 +0530

    Added response from bakedn

--- js/defined.js
+++ js/defined.js
@@ -4,8 +4,29 @@ function back(){
     $('#select').fadeIn()
 }
 function submit(){
-    console.log('submit')
-    $('#result').fadeIn()
-    $('#select').fadeOut()
+    if(!$('#crop').val()) return
+    console.log(marker1.getPosition().lat())
+    console.log(marker1.getPosition().lng())
+    console.log($('#crop').val())
+    $.ajax({
+        url:'http://localhost:5000/?lat='+marker1.getPosition().lat()+'&lng='+marker1.getPosition().lng()+'&crop='+$('#crop').val(),
+        beforeSend: function(){
+            $('#select').fadeOut()
+            $('#loader').fadeIn()
+        }
+    }).done(function(data){
+        console.log(data)
+        $('#totalyield').html(data.yield)
+        if(data.type==0)  $('#lowYield').show()
+        if(data.type==1)  $('#normalYield').show()
+        if(data.type==2)  $('#highYield').show()
+        $('#loader').fadeOut('fast')
+        $('#result').fadeIn('slow')
+    }).catch(function(data){
+        $('#loader').fadeOut('fast')
+        $('#select').fadeIn('slow')
+
+    })
+    
 
 }
