1eedc8d263f2a799f7f8ee205f0a4094d2b847f4
16BIT0127Prateek <prateekranjansingh@gmail.com>
2018-10-07 12:09:46 +0530

    Improve Agora Video call

--- js/defined.js
+++ js/defined.js
@@ -1,40 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').formSelect();
     $('.sidenav').sidenav();
-  });
-function back(){
+});
+
+function back() {
     console.log('back')
     $('#result').fadeOut()
     $('#select').fadeIn()
 }
-$(window).scroll(function() {
-    if($(this).scrollTop()>300 && $("#staticNav").hasClass("transparent")){
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 300 && $("#staticNav").hasClass("transparent")) {
         $("#staticNav").removeClass('z-depth-0')
         $("#staticNav").removeClass('transparent')
         $("#staticNav").addClass('pplr')
         $("#staticNav").addClass('z-depth-2')
-    }
-    else if($(this).scrollTop()<300 && $("#staticNav").hasClass("pplr")){
+    } else if ($(this).scrollTop() < 300 && $("#staticNav").hasClass("pplr")) {
         $("#staticNav").addClass('z-depth-0')
         $("#staticNav").addClass('transparent')
         $("#staticNav").removeClass('pplr')
         $("#staticNav").removeClass('z-depth-2')
     }
- });
-function scroll2(id){
+});
+
+function scroll2(id) {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#"+id).offset().top-100
+        scrollTop: $("#" + id).offset().top - 80
     }, 800);
 }
 
-function submit(){
-    if(!$('#crop').val()) return
+function submit() {
+    if (!$('#crop').val()) return
     console.log(marker1.getPosition().lat())
     console.log(marker1.getPosition().lng())
     console.log($('#crop').val())
     $.ajax({
-        url:'https://automata-agri.herokuapp.com/?lat='+marker1.getPosition().lat()+'&lng='+marker1.getPosition().lng()+'&crop='+$('#crop').val(),
-        beforeSend: function(){
+        url: 'https://automata-agri.herokuapp.com/?lat=' + marker1.getPosition().lat() + '&lng=' + marker1.getPosition().lng() + '&crop=' + $('#crop').val(),
+        beforeSend: function () {
             $('#select').fadeOut()
             $('#loader').fadeIn()
             $('#card-color').removeClass('green')
@@ -43,28 +44,28 @@ function submit(){
             $('#normalYield').hide()
             $('#highYield').hide()
         }
-    }).done(function(data){
+    }).done(function (data) {
         console.log(data)
         $('#totalyield').html(data.yield.toFixed(2))
-        if(data.type==0){
+        if (data.type == 0) {
             $('#lowYield').show()
             $('#card-color').addClass('red')
         }
-        if(data.type==1){
+        if (data.type == 1) {
             $('#normalYield').show()
             $('#card-color').addClass('green')
         }
-        if(data.type==2){
+        if (data.type == 2) {
             $('#highYield').show()
             $('#card-color').addClass('red')
         }
         $('#loader').fadeOut('fast')
         $('#result').fadeIn('slow')
-    }).catch(function(data){
+    }).catch(function (data) {
         $('#loader').fadeOut('fast')
         $('#select').fadeIn('slow')
 
     })
-    
+
 
 }
